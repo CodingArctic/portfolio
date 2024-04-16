526f25de5d666b9d77802df98b3c24b263393ec4
arctic <geyer.clyde@gmail.com>
2024-04-16 19:59:09 -0400

    and some other stuff

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Hey!</h1>
-      <h3 className={styles.subtitle}>I'm Clyde, a Web & Mobile Computing student at Rochester Institute of Technology.</h3>
+      <h2 className={styles.aboveTitle}>Hi there, my name is</h2>
+      <h1 className={styles.name}>Clyde Geyer</h1>
+      <div>
+        I'm a second year Web & Mobile Computing Major at RIT
+      </div>
     </main>
   );
 }
