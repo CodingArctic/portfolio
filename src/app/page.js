a299464ee8f9c1dce9596b3dcc7f2272ab263399
arctic <geyer.clyde@gmail.com>
2024-04-16 20:40:45 -0400

    ok a little more of something but i gotta do other things now

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,19 @@
-import Image from "next/image";
+import NavBar from './components/NavBar'
 
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h2 className={styles.aboveTitle}>Hi there, my name is</h2>
-      <h1 className={styles.name}>Clyde Geyer</h1>
-      <div>
-        I'm a second year Web & Mobile Computing Major at RIT
-      </div>
-    </main>
+    <>
+      <NavBar />
+      <main className={styles.main}>
+
+        <h2 className={styles.aboveTitle}>Hi there, my name is</h2>
+        <h1 className={styles.name}>Clyde Geyer</h1>
+        <div className={styles.blurb}>
+          I'm a second year <a href="https://www.rit.edu/study/web-and-mobile-computing-bs" target="_blank">Web & Mobile Computing Major</a> at <a className="link" href='https://rit.edu' target="_blank">RIT</a>
+        </div>
+      </main>
+    </>
   );
 }
